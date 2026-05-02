6f7d934f345647e8b0128dc725427645f04a142c
mosama <mosama88@hotmail.com>
2026-05-02 23:08:10 +0300

    first commit3

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
        <aside class="main-sidebar sidebar-dark-primary elevation-4">
            <!-- Brand Logo -->
-           <a href="index3.html" class="brand-link">
+           <a href="{{ route('dashboard') }}" class="brand-link">
                <img src="{{ asset('admin') }}/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
                    class="brand-image img-circle elevation-3" style="opacity: .8">
                <span class="brand-text font-weight-light">AdminLTE 3</span>
@@ -40,7 +40,7 @@
                with font-awesome or any other icon font library -->
 
                        <li class="nav-item">
-                           <a href="pages/widgets.html" class="nav-link active">
+                           <a href="{{ route('dashboard') }}" class="nav-link active">
                                <i class="nav-icon fas fa-tachometer-alt"></i>
                                <p>
                                    Dashboard
